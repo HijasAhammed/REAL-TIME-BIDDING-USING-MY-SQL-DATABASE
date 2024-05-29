44f1315df75abfa180744c1a86230b6152b80600
Hijas Ahammed <hijasahammedp@gmail.com>
2024-05-29 12:23:11 +0530

    authentication routes implemented

---
+++ Routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const authcontroller=require("../Controllers/authcontroller")
+const router=express.Router();
+
+
+router.post('/register',authcontroller.register);
+router.post('/login',authcontroller.login);
+
+module.expore=router;
